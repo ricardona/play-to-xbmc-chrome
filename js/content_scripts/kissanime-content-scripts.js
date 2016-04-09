b64c5ffdd32605637236ad92ff4c14e4c82527e8
Ricardo Cardona <ricardona@gmail.com>
2016-04-09 11:27:32 -0500

    Add support form kissanime.com

--- js/content_scripts/kissanime-content-scripts.js
+++ js/content_scripts/kissanime-content-scripts.js
@@ -1,7 +1,13 @@
+alert('chrome.extension.onMessage.addListener ENTER');
+
 chrome.extension.onMessage.addListener(    
     function(request, sender, sendResponse) {
+        alert('chrome.extension.onMessage.addListener');
+        console.log("Received message: " + request.action);
         var $video = $('video');
-	   if (request.action == "getVideoSrc") {
+        alert('Video ' + $video);
+        console.log('Video ' + $video);
+        if (request.action == "getVideoSrc") {
             var videoSrc = $video.attr('src');
             sendResponse({videoSrc: videoSrc});
         } else if (request.action == "onPlayback") {
