7567fb11a42d4c87e00f3887fdbe2c82d50d71fd
Ricardo Cardona <ricardona@gmail.com>
2016-04-09 11:34:53 -0500

    Remove debug messages

--- js/content_scripts/kissanime-content-scripts.js
+++ js/content_scripts/kissanime-content-scripts.js
@@ -1,12 +1,8 @@
-alert('chrome.extension.onMessage.addListener ENTER');
 
-chrome.extension.onMessage.addListener(    
+
+chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
-        alert('chrome.extension.onMessage.addListener');
-        console.log("Received message: " + request.action);
         var $video = $('video');
-        alert('Video ' + $video);
-        console.log('Video ' + $video);
         if (request.action == "getVideoSrc") {
             var videoSrc = $video.attr('src');
             sendResponse({videoSrc: videoSrc});
